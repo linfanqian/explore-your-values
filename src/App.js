7c984e663f99b62583685bb99750cbb4bd51c935
linfanqian <124747129+linfanqian@users.noreply.github.com>
2024-09-02 16:20:12 -0400

    Add old result retrieving feature

--- src/App.js
+++ src/App.js
@@ -1,119 +1,16 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import Deck from './Deck';
+import { values, importance } from './Const';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const values = [
-  { name: 'Acceptance', description: 'to be accepted as I am' },
-  { name: 'Accuracy', description: 'to be correct in my opinions and beliefs' },
-  { name: 'Achievement', description: 'to have important accomplishments' },
-  { name: 'Adventure', description: 'to have new and exciting experiences' },
-  { name: 'Art', description: 'to appreciate or express myself in art' },
-  { name: 'Attractiveness', description: 'to be physically attractive' },
-  { name: 'Authority', description: 'to be in charge of others' },
-  { name: 'Autonomy', description: 'to be self-determined and independent' },
-  { name: 'Beauty', description: 'to appreciate beauty around me' },
-  { name: 'Belonging', description: 'to have a sense of belonging, being part of' },
-  { name: 'Caring', description: 'to take care of others' },
-  { name: 'Challenge', description: 'to take on difficult tasks and problems' },
-  { name: 'Comfort', description: 'to have a pleasant and comfortable life' },
-  { name: 'Commitment', description: 'to make enduring, meaningful commitments' },
-  { name: 'Compassion', description: 'to feel and act on concern for others' },
-  { name: 'Complexity', description: 'to embrace the intricacies of life' },
-  { name: 'Compromise', description: 'to be willing to give and take in reaching agreements' },
-  { name: 'Contribution', description: 'to make a lasting contribution in the world' },
-  { name: 'Cooperation', description: 'to work collaboratively with others' },
-  { name: 'Courage', description: 'to be brave and strong in the face of adversity' },
-  { name: 'Courtesy', description: 'to be considerate and polite toward others' },
-  { name: 'Creativity', description: 'to create new things or ideas' },
-  { name: 'Curiosity', description: 'to seek out, experience and learn new things' },
-  { name: 'Dependability', description: 'to be reliable and trustworthy' },
-  { name: 'Diligence', description: 'to be thorough and conscientious in whatever I do' },
-  { name: 'Ecology', description: 'to live in harmony with the environment' },
-  { name: 'Excitement', description: 'to have a life full of thrills and stimulation' },
-  { name: 'Fame', description: 'to be known and recognized' },
-  { name: 'Family', description: 'to have a happy, loving family' },
-  { name: 'Faithfulness', description: 'to be loyal and true in relationships' },
-  { name: 'Fitness', description: 'to be physically fit and strong' },
-  { name: 'Flexibility', description: 'to adjust to new circumstances easily' },
-  { name: 'Forgiveness', description: 'to be forgiving of others' },
-  { name: 'Freedom', description: 'to be free from undue restrictions and limitations' },
-  { name: 'Friendship', description: 'to have close, supportive friends' },
-  { name: 'Fun', description: 'to play and have fun' },
-  { name: 'Generosity', description: 'to give what I have to others' },
-  { name: 'Genuineness', description: 'to act in a manner that is true to who I am' },
-  { name: 'God\'s Will', description: 'to seek and obey the will of God' },
-  { name: 'Growth', description: 'to keep changing and growing' },
-  { name: 'Health', description: 'to be physically well and healthy' },
-  { name: 'Honesty', description: 'to be honest and truthful' },
-  { name: 'Hope', description: 'to maintain a positive and optimistic outlook' },
-  { name: 'Humility', description: 'to be modest and unassuming' },
-  { name: 'Humor', description: 'to see the humorous side of myself and the world' },
-  { name: 'Imagination', description: 'to have dreams and see possibilities' },
-  { name: 'Independence', description: 'to be free from depending on others' },
-  { name: 'Industry', description: 'to work hard and well at my life tasks' },
-  { name: 'Integrity', description: 'to live my daily life in a way that is consistent with my values' },
-  { name: 'Intelligence', description: 'to keep my mind sharp and active' },
-  { name: 'Intimacy', description: 'to share my innermost experiences with others' },
-  { name: 'Justice', description: 'to promote fair and equal treatment for all' },
-  { name: 'Knowledge', description: 'to learn and contribute valuable knowledge' },
-  { name: 'Leadership', description: 'to inspire and guide others' },
-  { name: 'Leisure', description: 'to take time to relax and enjoy' },
-  { name: 'Loved', description: 'to be loved by those close to me' },
-  { name: 'Loving', description: 'to give love to others' },
-  { name: 'Mastery', description: 'to be competent in my everyday activities' },
-  { name: 'Mindfulness', description: 'to live conscious and mindful of the present moment' },
-  { name: 'Moderation', description: 'to avoid excesses and find a middle ground' },
-  { name: 'Monogamy', description: 'to have one close, loving relationship' },
-  { name: 'Music', description: 'to enjoy or express myself in music' },
-  { name: 'Non-Conformity', description: 'to question and challenge authority and norms' },
-  { name: 'Novelty', description: 'to have a life full of change and variety' },
-  { name: 'Nurturance', description: 'to encourage and support others' },
-  { name: 'Openness', description: 'to be open to new experiences, ideas and options' },
-  { name: 'Order', description: 'to have a life that is well-ordered and organized' },
-  { name: 'Passion', description: 'to have deep feelings about ideas, activities or people' },
-  { name: 'Patriotism', description: 'to love, serve and protect my country' },
-  { name: 'Pleasure', description: 'to feel good' },
-  { name: 'Popularity', description: 'to be well-liked by many people' },
-  { name: 'Practicality', description: 'to focus on what is practical, prudent, and sensible' },
-  { name: 'Protect', description: 'to protect and keep safe those I love' },
-  { name: 'Provide', description: 'to provide for and take care of my family' },
-  { name: 'Power', description: 'to have control over others' },
-  { name: 'Purpose', description: 'to have meaning and direction in my life' },
-  { name: 'Rationality', description: 'to be guided by reason, logic and evidence' },
-  { name: 'Realism', description: 'to see and act realistically and practically' },
-  { name: 'Responsibility', description: 'to make and carry out responsible decisions' },
-  { name: 'Risk', description: 'to take risks and chances' },
-  { name: 'Romance', description: 'to have intense, exciting love in my life' },
-  { name: 'Safety', description: 'to be safe and secure' },
-  { name: 'Self-Acceptance', description: 'to accept myself as I am' },
-  { name: 'Self-Control', description: 'to be disciplined ini my own actions' },
-  { name: 'Self-Esteem', description: 'to feel good about myself' },
-  { name: 'Self-Knowledge', description: 'to have a deep and honest understanding of myself' },
-  { name: 'Service', description: 'to be helpful and of service to others' },
-  { name: 'Sexuality', description: 'to have an active and satisfying sex life' },
-  { name: 'Simplicity', description: 'to live life simply, with minimal needs' },
-  { name: 'Solitude', description: 'to have time and space where I can be apart from others' },
-  { name: 'Spirituality', description: 'to grow and mature spiritually' },
-  { name: 'Stability', description: 'to have a life that stays fairly consistent' },
-  { name: 'Tolerance', description: 'to accept and respect those who differ from me' },
-  { name: 'Tradition', description: 'to follow respected patterns of the past' },
-  { name: 'Virtue', description: 'to live a morally pure and excellent life' },
-  { name: 'Wealth', description: 'to have plenty of money' },
-  { name: 'World Peace', description: 'to work to promote peace in the world' },
-]
-
-const importance = [
-  'Most Important to Me',
-  'Very Important to Me',
-  'Important to Me',
-  'Somewhat Important to Me',
-  'Not Important to Me',
-]
-
 function App() {
-  const [cards, setCards] = useState(values);
-  const [categories, setCategories] = useState({
+  const location = useLocation();
+  const { cards: initialCards, categories: initialCategories } = location.state || {};
+
+  const [cards, setCards] = useState(() => initialCards || values);
+  const [categories, setCategories] = useState(() => initialCategories || {
     [importance[0]]: [],
     [importance[1]]: [],
     [importance[2]]: [],
@@ -165,7 +62,7 @@ function App() {
     setCategories(updatedCategories);
 
     // Add the item back to the original cards array
-    setCards(prevCards => [...prevCards, item]);
+    setCards(prevCards => [item, ...prevCards]);
   };
 
   const openModal = (category) => {
@@ -182,6 +79,10 @@ function App() {
 
   return (
     <div className='container p-3'>
+      <div className='row pt-3 text-danger-emphasis bg-danger-subtle border border-danger-subtle rounded-3'>
+        <p>WARNING: Don't refresh the page, otherwise you will start from the beginning</p>
+      </div>
+
       <div className='row pb-3' name='count'>
         <div class="col">
           <div class="d-flex justify-content-between align-items-center">
@@ -195,6 +96,7 @@ function App() {
         </div>
       </div>
 
+      <h4>To Me, It's:</h4>
       <div className='row pb-5' name='categories'>
         {importance.map((cat, index) => (
           <div className='col' key={index}>
@@ -202,7 +104,7 @@ function App() {
             {categories[cat].length > 0 && (
               <Deck cards={categories[cat]} reverse={true} />
             )}
-            <button className="btn btn-outline-primary btn-sm" onClick={() => openModal(cat)}>See All</button>
+            <button className="btn btn-outline-primary btn-sm" onClick={() => openModal(cat)}>See List</button>
           </div>
         ))}
       </div>
--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,104 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { values } from './Const';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Home({ onStartClick, onCodeSubmit }) {
-  const [code, setCode] = useState('');
+function Home({ onStartClick }) {
+  const [file, setFile] = useState(null);
 
   let navigate = useNavigate();
   function onStartClick() {
     navigate('/app');
   }
 
+  const setFileChange = (event) => {
+    setFile(event.target.files[0]);
+  }
+
+  const parseFile = () => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const fileContent = event.target.result;
+      const lines = fileContent.split('\n');
+      const categories = {};
+      let currentCategory = null;
+      let usedCards = new Set();
+      let isValid = true;
+
+      for (const line of lines) {
+        if (line.trim().endsWith(':')) {
+          // New category detected
+          currentCategory = line.trim().slice(0, -1); // remove the colon and space at the end
+          if (!currentCategory) {
+            isValid = false;
+            break;
+          }
+          categories[currentCategory] = [];
+        } else if (line.trim() && currentCategory) {
+          // Parsing items belonging to the current category
+          const itemDetail = line.trim().split(' - ');
+          if (itemDetail.length === 2) {
+            // Check for valid name and description
+            const item = values.find(v => v.name === itemDetail[0] && v.description === itemDetail[1]);
+            if (!item) {
+              alert(`Value "${itemDetail[0]} - ${itemDetail[1]}" is invalid!`);
+              isValid = false;
+              break;
+            }
+
+            // Add this value to current category
+            categories[currentCategory].push(item);
+            usedCards.add(item.name);
+          } else {
+            isValid = false;
+            break;
+          }
+        } else if (line.trim() === '' && currentCategory) {  // line break between categories
+          continue;
+        } else {
+          isValid = false;
+          break;
+        }
+      };
+
+      if (!isValid) {
+        alert('File is invalid!');
+      } else {
+        const cards = values.filter(card => !usedCards.has(card.name));  // remaining cards
+        navigate('/app', { state: { cards, categories } });
+      }
+    };
+
+    reader.onerror = () => {
+      alert('File is invalid!');
+    };
+  
+    reader.readAsText(file); // Read the file as text
+  }
+
   return (
-    <div>
-      <h1>Welcome to the Value Identification Tool</h1>
-      <button onClick={onStartClick}>Let's Start</button>
-      <div>
-        <input
-          value={code}
-          onChange={e => setCode(e.target.value)}
-          placeholder="Enter your reference code"
-        />
-        <button onClick={() => onCodeSubmit(code)}>Revisit My Result</button>
+    <div className='vh-100 d-flex flex-column justify-content-center align-items-center'>
+      <div className='w-100 p-3'>
+        <h1 className='text-center'>Welcome to the Value Identification Tool</h1>
+      </div>
+
+      <div className='p-3'>
+        <button className="btn btn-primary" onClick={onStartClick}>🚀Click Me to Start</button>
+      </div>
+
+      <div className='p-3'>
+        <text className='fw-bold'>Or, revisit a previous result: </text>
+        <div class="input-group">
+          <input 
+            type="file" class="form-control" id="inputGroupFile04" accept=".txt"
+            aria-describedby="inputGroupFileAddon04" aria-label="Upload"
+            onChange={setFileChange}
+          />
+          <button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04" onClick={parseFile}>
+            Revisit Result
+          </button>
+        </div>
       </div>
     </div>
   );
